398ba711f4379a763b13222e3c9674a2b15dff79
luuizz <luu.izz@hotmail.com>
2024-09-02 21:39:38 -0300

    feature: inserido função para adicionar classe ativa e fixa do menu

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import "./scss/main.scss";
 import $ from "jquery";
 import "@phosphor-icons/web/regular";
 import { slidesTestimonials } from "./Slide.js";
+import { initMenu } from "./menu.js";
 
 $(document).ready(function () {
   slidesTestimonials.init();
+
+  initMenu();
 
   $("#js-year").text(new Date().getFullYear());
 });
---
+++ src/menu.ts
@@ -0,0 +1,59 @@
+import $ from "jquery";
+
+export function initMenu() {
+  $(".nav-menu a[href^='#']").click(function (e) {
+    e.preventDefault();
+
+    const id: string | undefined = $(this).attr("href");
+    const menuHeight: number = $("header").innerHeight() ?? 0;
+
+    if (id) {
+      const targetElement = $(id);
+      const targetOffset: number | undefined = targetElement.offset()?.top;
+
+      if (targetOffset !== undefined) {
+        $("html, body").animate(
+          {
+            scrollTop: targetOffset - menuHeight,
+          },
+          500
+        );
+      } else {
+        console.warn("Elemento não encontrado");
+      }
+    } else {
+      console.warn("Elemento não encontrado");
+    }
+  });
+
+  const activeLinks = "active";
+
+  $("section").each(function () {
+    const height = $(this).height() ?? 0,
+      offsetTop = $(this).offset()?.top ?? 0,
+      menuHeight = $(".menu").innerHeight() ?? 0,
+      id = $(this).attr("id"),
+      $itemMenu = $('a[href="#' + id + '"]');
+
+    $(window).scroll(function () {
+      const scrollTop = $(window).scrollTop() ?? 0;
+      if (
+        offsetTop - menuHeight < scrollTop &&
+        offsetTop + height - menuHeight > scrollTop
+      ) {
+        $itemMenu.addClass(activeLinks);
+      } else {
+        $itemMenu.removeClass(activeLinks);
+      }
+    });
+  });
+
+  $(window).on("scroll", () => {
+    const header = $("header");
+    if (window !== null) {
+      window.scrollY > 50
+        ? header.addClass("fixed")
+        : header.removeClass("fixed");
+    }
+  });
+}
